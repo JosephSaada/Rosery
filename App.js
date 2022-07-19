5143991f5e97f247a8f02b05c1e07e0ebf5efcc5
Joseph Saada <jsaada2003@gmail.com>
2022-07-18 22:02:04 -0700

    removed the most annyoing fucking warning, will remove changes in deployment

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react'; 
 import StackNavigator from './StackNavigator'; 
 import {NavigationContainer} from '@react-navigation/native';   
+import { LogBox } from "react-native";
 
-export default function App() { 
-  return (   
+export default function App() {  
+  LogBox.ignoreLogs(["EventEmitter.removeListener"]); 
+  return (    
     <NavigationContainer>    
      <StackNavigator />
     </NavigationContainer>
